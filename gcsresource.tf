8ec54aeb6ced6ca3b0da10c4dc60ca00969920cf
rohitraikar565 <61474353+rohitraikar565@users.noreply.github.com>
2022-04-27 12:36:37 +0530

    Update gcsresource.tf

--- gcsresource.tf
+++ gcsresource.tf
@@ -11,8 +11,9 @@ provider "google" {
   project     = "inframod-training"
   zone        = "asia-south1-a"
 }
-resource "google_storage_bucket" "static-site" {
+resource "google_storage_bucket" "stat-bucket" {
   name          = "image-store"
   location      = "asia-south1"
   force_destroy = true
+  uniform_bucket_level_access = true
 }
