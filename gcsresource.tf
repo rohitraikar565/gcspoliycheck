b4c71d67bb9a51c764d826e7e91a3f12d454ec5d
rohitraikar565 <61474353+rohitraikar565@users.noreply.github.com>
2022-05-06 16:54:12 +0530

    Update gcsresource.tf

--- gcsresource.tf
+++ gcsresource.tf
@@ -4,7 +4,9 @@ provider "google" {
 }
 resource "google_storage_bucket" "static-site" {
   name          = "image-store.com"
+  location      = "EU"
   force_destroy = true
+  
 
   uniform_bucket_level_access = true
 }
