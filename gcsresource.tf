caedfbcba6ce3726b240820d5950a64aeccba223
rohitraikar565 <61474353+rohitraikar565@users.noreply.github.com>
2022-05-06 15:59:45 +0530

    Update gcsresource.tf

--- gcsresource.tf
+++ gcsresource.tf
@@ -1,8 +1,8 @@
 provider "google" {
   project     = "tesing-342306"
-  zone        = "asia-south1"
+  zone        = "asia-south1-a"
 }
-resource "google_storage_bucket" "stat-bucket" {
+resource "google_storage_bucket" "xpcs" {
   name          = "image-store"
   location      = "asia-south1-a"
   force_destroy = true
@@ -12,7 +12,7 @@ resource "google_storage_bucket" "stat-bucket" {
     }
 }
 
-resource "google_logging_project_bucket_config" "stat-bucket" {
+resource "google_logging_project_bucket_config" "xpcs" {
     project     = "niveus-delivery-products"
     location  = "asia-south1-a"
     retention_days = 30
