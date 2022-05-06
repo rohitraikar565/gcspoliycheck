45ece3e14db3d6cd321b2aebd4bce98dd24a0f78
rohitraikar565 <61474353+rohitraikar565@users.noreply.github.com>
2022-05-06 15:31:11 +0530

    Update gcsresource.tf

--- gcsresource.tf
+++ gcsresource.tf
@@ -1,7 +1,6 @@
 provider "google" {
   project     = "tesing-342306"
   zone        = "asia-south1"
-  credentials = "tesing-342306-f93e1b4153bc.json"
 }
 resource "google_storage_bucket" "stat-bucket" {
   name          = "image-store"
